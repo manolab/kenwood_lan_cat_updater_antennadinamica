b4887479413f07b7dc2f5f6513ffd5e34dd4a879
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2022-12-18 17:53:12 +0100

    Remove unused code

--- errors.py
+++
@@ -1,6 +0,0 @@
-class AuthException(Exception):
-    """Raised when authentication fails"""
-
-    def __init__(self, errorcode="Unknown") -> None:
-        self.message = f"Authentication failed with code {errorcode}"
-        super().__init__(self.message, errorcode)
