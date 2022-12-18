429118407d57e2ae7b44a0a1bf027dbe476003a3
Mat <myemail@email.com>
2022-12-18 16:37:51 +0100

    Just adding a small refactoring, some error handling and writing to file on exit

---
+++ README.md
@@ -0,0 +1,14 @@
+## cat_updater_lan890.py
+
+### Run (using `pipenv`)
+
+```shell
+pipenv run python main.py --host <host> --port <port> --output <output_path> --user <username> --password <password>
+
+## E.g.
+pipenv run python main.py --host "192.168.1.16" \
+  --port 60000 \
+  --output "/usr/local/share/ad/frequency.txt" \
+  --user "admin" \
+  --password "normando"
+```
--- cat_updater_lan890.py
+++ cat_updater_lan890.py
@@ -1,50 +1,69 @@
 # boh
 
+import atexit
 import socket
 import time
-
-HOST="192.168.1.16"
-PORT=60000
-user="admin"
-password="normando"
-
-cat_status_fname = "/usr/local/share/ad/cat_status.txt"
-frequency_fname  = "/usr/local/share/ad/frequency.txt"
-
-# intanto metto -5 che dovrebbe essere 'non collegato'
-#t = open(frequency_fname, "w")
-#t.write('-5')
-#t.close()
-#0 è cat ok
+from errors import AuthException
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    s.connect((HOST, PORT))
-    s.sendall(b"##CN;")
-    data = s.recv(1024)
-    if data.decode('utf-8') == '##CN1;':
-        print("Connected")
-        s.sendall(b"##ID00508adminnormando;")
-        data = s.recv(1024)
-        if data.decode('utf-8') == '##ID1;':
-            print("Authenticated")
+def do_auth(my_socket, user, password) -> None:
+    """Authentication"""
 
-            s.sendall(b"AI0;")
-            #data = s.recv(1024)
-            #print(f"Received {data!r}")
+    auth = f"##ID00508{user}{password};"
+    my_socket.sendall(auth.encode('utf-8'))
+    socket_data = my_socket.recv(1024)
+    if socket_data.decode('utf-8') != '##ID1;':
+        raise AuthException(socket_data.decode('utf-8'))
 
-            while True:
-                s.sendall(b"FA;")
-                data = s.recv(1024)
-                #print(f"Received {data!r}")
-                frequency = data.decode('utf-8')[2:].lstrip("0")[0:-3]+'00'
-                print(frequency)
-                t = open(frequency_fname, "w")
-                t.write(frequency)
-                t.close()
-                time.sleep(1)
+    print("Authenticated")
+    my_socket.sendall(b"AI0;")
+    # data = s.recv(1024)
+    # print(f"Received {data!r}")
 
-except socket.timeout:
-    # qui
-    print("ciao")
+
+def do_connection(my_socket, host, port) -> None:
+    """Socket connection"""
+
+    my_socket.connect((host, int(port)))
+    my_socket.sendall(b"##CN;")
+    socket_data = my_socket.recv(1024)
+    if socket_data.decode('utf-8') != '##CN1;':
+        raise ConnectionError
+
+
+def do_other_stuff(my_socket) -> str:
+    """Do other stuff"""
+
+    my_socket.sendall(b"FA;")
+    socket_data = my_socket.recv(1024)
+    decoded = socket_data.decode('utf-8')[2:].lstrip("0")[0:-3] + '00'
+    print(decoded)
+    return decoded
+
+
+def write_results(path, results) -> None:
+    """Write results"""
+
+    file = open(path, "w+", encoding="utf-8")
+    file.write(results)
+
+
+def start(host, port, output, user, password):
+    """Main logic"""
+
+    resulting_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    do_connection(resulting_socket, host, port)
+    do_auth(resulting_socket, user, password)
+    while True:
+        results = do_other_stuff(resulting_socket)
+        write_results(output, results)
+        time.sleep(1)
+
+
+def exit_handler():
+    print('\nThe application is terminating... writing to file...\n')
+    file = open('test_on_exit_file', 'w+')
+    file.write('Bye!')
+
+
+atexit.register(exit_handler)
